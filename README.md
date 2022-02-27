1dbbc93b51e50ca34d0f309b11995786b23aba8b
Me <me@homeserver.rumah.terdidik.com>
2022-02-28 03:20:07 +0700

    Add Systemd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gclouddns
 Update Google Cloud DNS of IP Address Domain (Dynamic DNS)
+
+## Preparation
+please install GCloud SDK and run "gcloud auth login" to set authentication
+create a .env file for custom environment or direct edit variables in gclouddns.sh
--- gclouddns.sh
+++ gclouddns.sh
@@ -11,17 +11,10 @@ then
 fi
 
 RECORDS=$(gcloud dns record-sets list --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE --name=$DNS_RECORD. --type=A)
-
 NEW_IP=$(curl -s https://ifconfig.me)
 
-echo $RECORDS
-echo $NEW_IP
 
 if [[ "$RECORDS" != *"$NEW_IP"* ]];then
   echo "Changing A to $NEW_IP"
-  #gcloud dns record-sets transaction start --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE
   gcloud dns record-sets update $DNS_RECORD --rrdatas=$NEW_IP --type=A --ttl=300 --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE
-  #gcloud dns record-sets transaction execute --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE
 fi
-
-#NEW_IP=$(curl -s https://ifconfig.me)
