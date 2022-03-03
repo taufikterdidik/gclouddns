5aba77e763c35b5517d68cd72fa7e4b58a5f587d
Taufik Fahrudin <taufik@terdidik.com>
2022-03-04 00:11:00 +0700

    template systemd

--- gclouddns.sh
+++ gclouddns.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+### VERSION 1.0
+### Author : <Taufik Fahrudin> taufik@terdidik.com
+
 ACCOUNT=example@terdidik.com
 PROJECT=example
 DNS_ZONE=terdidik-com
@@ -11,11 +14,30 @@ then
   export $(cat "$DIR/.env" | xargs)
 fi
 
-RECORDS=$(gcloud dns record-sets list --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE --name=$DNS_RECORD. --type=A)
-NEW_IP=$(curl -s https://ifconfig.me)
+if [ -f "/etc/gclouddns.conf" ]; then
+  export $(cat "/etc/gclouddns.conf" | xargs)
+fi
 
+while getopts c: flag
+do
+  case "${flag}" in
+    c) CONFIG=${OPTARG};;
+  esac
+done
+
+if [ "$CONFIG" != "" ]; then
+  export $(cat "/etc/gclouddns/${CONFIG}.conf" | xargs)
+fi
+
+RECORDS=$(gcloud dns record-sets list --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE --name=$DNS_RECORD. --type=A)
+
+NEW_IP=$(curl -s http://ifconfig.me | sed 's/ *$//g')
+if [[ ! "$NEW_IP" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]];then
+  NEW_IP=$(curl -s http://ifconfig.co | sed 's/ *$//g')
+fi
 
 if [[ "$RECORDS" != *"$NEW_IP"* && "$NEW_IP" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]];then
-  echo "Changing A to $NEW_IP"
-  gcloud dns record-sets update $DNS_RECORD --rrdatas=$NEW_IP --type=A --ttl=300 --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE
+  logger "Changing type A $DNS_RECORD to $NEW_IP"
+  ATTL="${TTL:=300}"
+  gcloud dns record-sets update $DNS_RECORD --rrdatas=$NEW_IP --type=A --ttl=$ATTL --account=$ACCOUNT --project=$PROJECT --zone=$DNS_ZONE
 fi
